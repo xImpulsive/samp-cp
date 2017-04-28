44efb053da815e16f6cee131792b89240ca3ecbc
raphaelpa <raphael.papazikas@gmail.com>
2017-04-28 22:05:33 +0200

    clean laravel structure

---
+++ resources/lang/de/acp.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    "title" => "Administrative Oberfläche",
+];
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// @TODO before filter!
+Route::group(["prefix" => "/acp"], function() {
+
+    Route::get("/", [
+        "as" => "acp.home",
+        "uses" => "AdminController@index",
+    ]);
+
+});
