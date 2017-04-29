8565071fcd92dcf210999e6dff3d52a1bcfd4fbc
derrobin154 <robin@robin154.de>
2017-04-30 01:08:58 +0200

    Editing to Display the Page

--- app/Models/GitVersion.php
+++ app/Models/GitVersion.php
@@ -5,7 +5,7 @@ class GitVersion
 {
     public static function getVersion($branch = "master")
     {
-        if ( $hash = file_get_contents( sprintf( base_path() . '.git/refs/heads/%s', $branch ) ) ) {
+        if ( $hash = file_get_contents( sprintf('/.git/refs/heads/%s', $branch ) ) ) {
             return $hash;
         } else {
             return false;
