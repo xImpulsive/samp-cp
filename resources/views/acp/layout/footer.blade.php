dcb62d59e57bdfccceaedfc1274db9132bc7150c
derrobin154 <robin@robin154.de>
2017-04-30 01:27:42 +0200

    Add a Link to Git im Version footer

--- resources/views/acp/layout/footer.blade.php
+++ resources/views/acp/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> Git: <a href="https://github.com/xImpulsive/samp-cp/commit/{{ HTML::version() }}">{{ HTML::version() }}</a>
+       <b>Git Version: </b><a href="https://github.com/xImpulsive/samp-cp/commit/{{ HTML::version() }}">{{ HTML::version() }}</a>
     </div>
-    <strong>Copyright &copy; 2017 <a href="#">SAMP-CP</a>.</strong> All rights
+    <strong>Copyright &copy; {{ date("Y") }} <a href="https://samp-cp.com">SAMP-CP</a>.</strong> All rights
     reserved.
 </footer>
